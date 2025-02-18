f492ecedf8531919edd80c5d2c3256760629ac3e
Lee bada <dlqkek1113@naver.com>
2025-02-18 13:57:05 +0900

    20250218

--- 연습문제/0218/이바다.py
+++ 연습문제/0218/이바다.py
@@ -1,3 +1,4 @@
+import datetime
 days = ['월','화','수','목','금','토','일']
 day1 = datetime.date(2025,2,18)
 day2 = datetime.date(2025,5,5)
@@ -7,16 +8,14 @@ print("그날은" + days[day2.weekday()] + "요일 입니다.")
 
 days = ['월','화','수','목','금','토','일']
 day1 = datetime.date(2025,2,18)
-day2 = datetime.date(2025,5,8)
-diff = day2 - day1
+day3 = datetime.date(2025,5,8)
+diff = day3 - day1
 print("어버이 날까지 남은 기간:", diff.days, "일")
-print("그날은" + days[day2.weekday()] + "요일 입니다.")
+print("그날은" + days[day3.weekday()] + "요일 입니다.")
 
 days = ['월','화','수','목','금','토','일']
 day1 = datetime.date(2025,2,18)
-day2 = datetime.date(2025,12,25)
-diff = day2 - day1
+day4 = datetime.date(2025,12,25)
+diff = day4 - day1
 print("크리스마스 날까지 남은 기간:", diff.days, "일")
-print("그날은" + days[day2.weekday()] + "요일 입니다.") 
-
-.
+print("그날은" + days[day4.weekday()] + "요일 입니다.")
